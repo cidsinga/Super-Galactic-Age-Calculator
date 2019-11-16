598ac4d376ac93f0a4d6781cd7f059ecaa417bb2
cidsinga <56556816+cidsinga@users.noreply.github.com>
2019-11-15 16:41:04 -0800

    still looking for the missing piece on my functions inside my class

--- src/galactic.js
+++ src/galactic.js
@@ -3,31 +3,29 @@ export function earthToMercury(input) {
 
 };
 
-export function mercAge(age) {
-  return age * .24;
-};
+// export function mercAge(age) {
+//   return age * .24;
+// };
 
 // work on classes below -----------------------
-// class GalacticBeing {
-//   constructor (age, le) {
-//     this.age = age;
-//     this.lifeExpectancy = le;
-//
-//   }
-//   mercAge(age) {
-//     return this.age * .24;
-//   }
-//   venusAge() {
-//     return this.age * .62;
-//   }
-//   marsAge() {
-//     return this.age * 1.88;
-//   }
-//   jupiterAge() {
-//     return this.age * 11.86;
-//   }
-//
-// };
-//
-// let galacticBeing1 = new GalacticBeing(10);
-// galacticBeing1.mercAge();
+export class GalacticBeing {
+  constructor (age) {
+    this.age = age;
+  }
+  mercAge() {
+    return this.age * .24;
+  }
+  // venusAge() {
+  //   return this.age * .62;
+  // }
+  // marsAge() {
+  //   return this.age * 1.88;
+  // }
+  // jupiterAge() {
+  //   return this.age * 11.86;
+  // }
+
+};
+
+let galacticBeing1 = new GalacticBeing(10);
+galacticBeing1.mercAge();
--- src/oldobjectstyle.js
+++
@@ -1,9 +0,0 @@
-function GalacticBeing(age) {
-  let arr = [];
-  this.age = age;
-
-}
-
-GalacticBeing.prototype.mercAge = function (age) {
-  arr.push(this.age * .24);
-};
